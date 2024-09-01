d5242ab59f4063f790ea063f37259330d6ffd12e
dipAlejandro <alejandrodipdip@gmail.com>
2024-09-01 15:35:11 -0300

    fix(js/scripts.js): Change public API key for Emailjs
    feat(index.html, js/scripts.js): Add alert in contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,53 @@
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-      publicKey: "OHnIcnmuMn-UzIT2K",
-    });
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "8wX7E5R_OaXdkU5Ya",
+  });
 })();
 
-window.onload = function() {
+function dinamicContactAlert(alertClass, title, body) {
+  const alertElement = document.getElementById("contactAlert");
+  alertElement.classList.remove("d-none");
 
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+  // Comprobar el alert class anterior
+  if (alertElement.classList.contains("alert-danger")) {
+    alertElement.classList.remove("alert-danger");
+  } else if (alertElement.classList.contains("alert-success")) {
+    alertElement.classList.remove("alert-success");
+  }
 
-        const from_email = document.getElementById("from_email").value;
+  alertElement.classList.add(alertClass);
 
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'template_contact_form', this)
-            .then(function () {
-                alert('The message was sent successfully!!. I will reply to you as soon as possible.');
-            },function (error) {
-               alert('FAILED...' + JSON.stringify(error));
-            });
-    });
+  alertElement.querySelector("h4").textContent = title;
+  alertElement.querySelector("p").textContent = body;
 }
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("contact_service", "template_contact_form", this).then(
+        function () {
+          dinamicContactAlert(
+            "alert-success",
+            "Your message was sent successfully!",
+            "I will reply to you as soon as possible."
+          );
+          document.getElementById("contact-form").reset();
+        },
+        function (error) {
+          dinamicContactAlert(
+            "alert-danger",
+            "Oops!",
+            "Failed to send message. Please try again."
+          );
+          document.getElementById("contact-form").reset();
+        }
+      );
+    });
+
+  const alertList = document.querySelectorAll(".alert");
+  alertList.forEach((element) => new bootstrap.Alert(element));
+};
